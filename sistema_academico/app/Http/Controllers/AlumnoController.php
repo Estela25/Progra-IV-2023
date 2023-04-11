efdb2797a948d23f712d15b532d48cf9db27584d
Estela25 <USIS035821@ugb.edu.sv>
2023-04-11 17:23:25 -0600

    sistema academico

---
+++ sistema_academico/app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Alumno;
+use Illuminate\Http\Request;
+
+class AlumnoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //select * from alumnos;
+        return alumno::get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)//post
+    {
+        //inserte into alumnos...
+        Alumno::create($request->all());
+        return response()->json(['msg'=>'ok'],200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Alumno $alumno)
+    {
+        //select * from alumno where idAlumno=?
+        return $alumno;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Alumno $alumno)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Alumno $alumno)
+    {
+        //update alumno set? where id=?
+        $alumno->update($request->all());
+        return response()->json(['msg'=>'ok'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Alumno $alumno)
+    {
+        //delete alumnos from alumnos where id=?
+        $alumno->delete();
+        return response()->json(['msg'=>'ok'], 200);
+    }
+}
